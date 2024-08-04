63b2e6a1fcca158a41ca03a166bf099258d17c66
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-08-04 20:36:16 +0530

    remove item in admin

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,7 +2,10 @@
 import React, { useEffect, useState } from 'react'
 import '../Add/Add.css'
 import { assets, url } from '../../assets/assets'
+import axios from "axios"
+import { toast } from 'react-toastify'
 const Add = () => {
+  const url ="http://localhost:4000"
   const [image,setImage] =useState(false);
   const[data,setData]= useState({
     name:"",
@@ -15,12 +18,36 @@ const Add = () => {
     const value =event.target.value;
     setData(data=>({...data,[name]:value}))
   }
+  const onSubmitHandler =async(event)=>{
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('name',data.name);
+    formData.append('description',data.description);
+    formData.append('price',Number(data.price));
+    formData.append('category',data.category);
+    formData.append('image',image);
+    const response = await axios.post(`${url}/api/food/add`,formData)
+    if (response.data.success){
+        
+      setData({
+        name:"",
+        description:"", 
+        price:"",
+        category:"Salad"
+      })
+      setImage(false)
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
  /* useEffect(()=>{
     console.log(data);
   },[data])*/
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
@@ -36,7 +63,7 @@ const Add = () => {
 
             <div className="add-product-description flex-col">
               <p>Description</p>
-              <textarea onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write content here' required />
+              <textarea onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write content here' />
             </div>
 
             <div className="add-category-price ">
