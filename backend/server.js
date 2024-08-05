87f70f4733e3ca5d24e800baaedd981b4224cbda
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-08-05 23:14:11 +0530

    server port

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config.js'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 //app config
 const app = express()
-const port = 4000
+const port =process.env.PORT|| 4000
 //middleware
 app.use (express.json())
 app.use(cors())
@@ -18,6 +19,8 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+
 app.get("/",(req,res)=>{
   res.send("hello world")
 })
